51439c2182d2baea2e13f8d209a78ff45481b662
LynchC2 <conor.l.lynch@ppb.com>
2023-01-26 16:25:44 +0000

    2nd Commit 2

--- second tab.py
+++ second tab.py
@@ -1,5 +1,21 @@
-print ('2nd file')
+import pandas as pd
+import numpy as np
 
-print ('bye')
+canadian_youtube = pd.read_csv("CAvideos.csv")
+british_youtube = pd.read_csv("GBvideos.csv")
 
-"""Adding some docstring"""
+concat_data = pd.concat([canadian_youtube, british_youtube])
+
+left = canadian_youtube.set_index(['title', 'trending_date'])
+right = british_youtube.set_index(['title', 'trending_date'])
+
+join_data = left.join(right, lsuffix='_CAN', rsuffix='_UK')
+
+merged_data= pd.merge(left,right,on='video_id')
+
+print(left.shape, right.shape)
+print(join_data.shape, merged_data.shape)
+print("sausage")
+
+
+
